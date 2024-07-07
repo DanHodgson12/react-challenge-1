428eca645f7ca4d68f1e3cc7a9db5700eeb28dd0
DanHodgson12 <danhodgson12@gmail.com>
2024-07-07 10:48:57 +0100

    feat: added componentDidMount function to display loader

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -1,32 +1,39 @@
-import React, { Component } from 'react';
-import css from './css/Content.module.css';
-import postsData from '../posts.json';
+import React, { Component } from "react";
+import css from "./css/Content.module.css";
+import postsData from "../posts.json";
 import PostItem from "./PostItem";
+import Loader from "./Loader";
 
 export class Content extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            isLoaded: false
+        }
     }
 
+    componentDidMount() {
+        setTimeout(()=>{
+            this.setState({
+                isLoaded: true,
+            })
+        }, 2000)
+    }
+    
     render() {
         return (
             <div className={css.Content}>
-                <div className={CSS.TitleBar}>
-                    <h1>My Posts</h1>
+                
+                <div className={css.TitleBar}>
+                    <h1>My Photos</h1>
                 </div>
-                <div className={CSS.SearchResults}>
-                    {/* {
-                        postsData.savedPosts.map((post) => {
-                            return <div className={css.SearchItem} key={post.title}>
-                                <p>{post.title}</p>
-                                <p>{post.name}</p>
-                                <img src={post.image} alt="random" />
-                                <p>{post.description}</p>
-                            </div>
-                        })
-                    } */}
 
-                    <PostItem savedPosts={postsData.savedPosts} />
+                <div className={css.SearchResults}>
+                    {
+                        this.state.isLoaded ?
+                        <PostItem savedPosts={postsData.savedPosts} />
+                        : <Loader />
+                    }
                 </div>
             </div>
         )
