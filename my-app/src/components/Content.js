dec350755567641aeb0607cb77444a8ba12b8a85
DanHodgson12 <danhodgson12@gmail.com>
2024-07-08 18:38:16 +0100

    added search bar and filtering logic

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -8,30 +8,57 @@ export class Content extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            isLoaded: false
+            isLoaded: false,
+            posts: [],
         }
     }
 
     componentDidMount() {
-        setTimeout(()=>{
-            this.setState({
-                isLoaded: true,
-            })
-        }, 2000)
+        // setTimeout(()=>{
+        //     this.setState({
+        //         isLoaded: true,
+        //         posts: postsData.savedPosts,
+        //     })
+        // }, 2000)
+        this.setState({
+            isLoaded: true,
+            posts: postsData.savedPosts,
+        })
     }
     
+    handleChange = (e) => {
+        const name = e.target.value.toLowerCase();
+        const filteredPosts = postsData.savedPosts.filter((post) => {
+            return post.name.toLowerCase().includes(name)
+        })
+
+        this.setState({
+            posts: filteredPosts
+        })
+    }
+
     render() {
         return (
             <div className={css.Content}>
                 
                 <div className={css.TitleBar}>
                     <h1>My Photos</h1>
+                    <form>
+                        <label htmlFor="searchInput">Search:</label>
+                        <input
+                            type="search"
+                            placeholder="By Author"
+                            id="searchInput"
+                            onChange={(e) => this.handleChange(e)}
+                        />
+                        <h4>Posts found: {this.state.posts.length}</h4>
+                    </form>
                 </div>
 
                 <div className={css.SearchResults}>
                     {
                         this.state.isLoaded ?
-                        <PostItem savedPosts={postsData.savedPosts} />
+                        <PostItem savedPosts={this.state.posts} />
                         : <Loader />
                     }
                 </div>
